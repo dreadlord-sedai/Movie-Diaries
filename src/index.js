12c99473ff4acafdef9fd93663600f04030fc4c7
dreadlord-sedai <dahamifabbio@gmail.com>
2025-11-06 11:52:09 +0530

    Fetch data using OMDB API

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import StarRating from './StartRating';
-// import './index.css';
-// import App from './App';
+import './index.css';
+import App from './App-1';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating maxRating={5} messages={["Terrible", "Bad", "OK", "Good", "Excellent"]}/>
-    <StarRating maxRating={10} color="red" size={48} defaultRating={3}/>
+    <App />
+    {/* <StarRating maxRating={5} messages={["Terrible", "Bad", "OK", "Good", "Excellent"]}/>
+    <StarRating maxRating={10} color="red" size={48} defaultRating={3}/> */}
   </React.StrictMode>
 );
 
